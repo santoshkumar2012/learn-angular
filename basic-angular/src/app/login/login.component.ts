7d2ba8ba9adc5d6734b702e9d0852d08cdd4f151
Santosh Kumar <web.santo2012@gmail.com>
2025-02-13 01:18:24 +0530

    Learn Pipe Binding in Angular 19

--- basic-angular/src/app/login/login.component.ts
+++ basic-angular/src/app/login/login.component.ts
@@ -1,11 +1,24 @@
+import { LowerCasePipe, TitleCasePipe, UpperCasePipe } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  imports: [LowerCasePipe, TitleCasePipe, UpperCasePipe],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
+
+  person_name = "Anjali Kumari"
+
+  company_details = {
+    name: "P & N Brothers",
+    company_address: "New Delhi",
+    company_length: 50,
+    company_owners: "Santosh Kumar"
+  }
+
+ 
+
 
 }
