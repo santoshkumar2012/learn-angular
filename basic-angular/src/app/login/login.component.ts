515736859f0cb5147e5cab91ef145a64dcedc33e
Santosh Kumar <web.santo2012@gmail.com>
2025-02-13 11:10:14 +0530

    Finish pipes with data binding

--- basic-angular/src/app/login/login.component.ts
+++ basic-angular/src/app/login/login.component.ts
@@ -3,22 +3,28 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-login',
-  imports: [LowerCasePipe, TitleCasePipe, UpperCasePipe],
+  imports: [LowerCasePipe, UpperCasePipe],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
 
-  person_name = "Anjali Kumari"
+  name = "Santosh Kumar"
 
-  company_details = {
-    name: "P & N Brothers",
-    company_address: "New Delhi",
-    company_length: 50,
-    company_owners: "Santosh Kumar"
+  company_profile = {
+    company_name: "P & N Brothers",
+    company_length: 50-2000,
+    company_address: "204 Apt Sathvik Apartment"
   }
 
- 
+  employees = [
+    { id:1, name:"Virendra Prasad", country:"Delhi", },
+    { id:2, name:"Santosh Kumar", country:"Pune", },
+    { id:3, name:"Anjali Kumari", country:"Delhi", },
+    { id:4, name:"Pragyansh", country:"Delhi", },
+    { id:5, name:"Nityansh", country:"Delhi", },
+    { id:6, name:"Sushila Deve", country:"Delhi", },
+  ]
 
 
 }
