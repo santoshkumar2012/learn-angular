c37c497c491832093c84b06f5a7519d9ef61f834
Santosh Kumar <web.santo2012@gmail.com>
2025-02-14 22:08:21 +0530

    updated pipe file

--- basic-angular/src/app/login/login.component.ts
+++ basic-angular/src/app/login/login.component.ts
@@ -1,30 +1,14 @@
-import { LowerCasePipe, TitleCasePipe, UpperCasePipe } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-login',
-  imports: [LowerCasePipe, UpperCasePipe],
+  imports: [],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
 
-  name = "Santosh Kumar"
-
-  company_profile = {
-    company_name: "P & N Brothers",
-    company_length: 50-2000,
-    company_address: "204 Apt Sathvik Apartment"
-  }
-
-  employees = [
-    { id:1, name:"Virendra Prasad", country:"Delhi", },
-    { id:2, name:"Santosh Kumar", country:"Pune", },
-    { id:3, name:"Anjali Kumari", country:"Delhi", },
-    { id:4, name:"Pragyansh", country:"Delhi", },
-    { id:5, name:"Nityansh", country:"Delhi", },
-    { id:6, name:"Sushila Deve", country:"Delhi", },
-  ]
+ 
 
 
 }
---
+++ basic-angular/src/app/pipe/pipe.component.ts
@@ -0,0 +1,28 @@
+import { LowerCasePipe, TitleCasePipe, UpperCasePipe } from '@angular/common';
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-pipe',
+  imports: [LowerCasePipe, UpperCasePipe],
+  templateUrl: './pipe.component.html',
+  styleUrl: './pipe.component.css'
+})
+export class PipeComponent {
+
+  name = "Santosh Kumar"
+  company_profile = {
+    company_name: "P & N Brothers",
+    company_length: 50-2000,
+    company_address: "204 Apt Sathvik Apartment"
+  }
+
+  employees = [
+    { id:1, name:"Virendra Prasad", country:"Delhi", },
+    { id:2, name:"Santosh Kumar", country:"Pune", },
+    { id:3, name:"Anjali Kumari", country:"Delhi", },
+    { id:4, name:"Pragyansh", country:"Delhi", },
+    { id:5, name:"Nityansh", country:"Delhi", },
+    { id:6, name:"Sushila Deve", country:"Delhi", },
+  ]
+
+}
