f6e3a3ad4e60eac43e59232ade6f539a3748145b
Santosh Kumar <web.santo2012@gmail.com>
2025-03-27 13:44:58 +0530

    Updated programme

---
+++ basic-angular/src/app/javascript/javascript.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-javascript',
+  imports: [],
+  templateUrl: './javascript.component.html',
+  styleUrl: './javascript.component.css'
+})
+export class JavascriptComponent {
+
+  constructor(){
+  }
+
+}
---
+++ basic-angular/src/app/programme.js
@@ -0,0 +1,3 @@
+
+var a = 15;
+console.log('Check--->', a );
